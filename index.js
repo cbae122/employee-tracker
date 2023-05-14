ec3e6257d9e4911efcb5dfdd38b683a0fa470fe8
cbae <xchris.bae@gmail.com>
2023-05-13 18:49:25 -0700

    added query to show on inquirer

--- index.js
+++ index.js
@@ -20,13 +20,14 @@ const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// // Express middleware
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
 // Connect to database
 const dbConnect = mysql.createConnection(
@@ -44,7 +45,8 @@ const dbConnect = mysql.createConnection(
 
 dbConnect.connect((err) => {
     if (err) throw err;
-    beforeDb()
+    console.log('connected as id ' + dbConnect.threadId);
+    beforeDb();
 });
 
 beforeDb = () => {
@@ -65,20 +67,85 @@ const startDb = () => {
         message: 'What would you like to do?',
         choices: [
             'View All Employees',
-            'View All Employees By Department',
-            'View All Employees By Manager',
-            'Add Employee',
-            'Remove Employee',
-            'Update Employee Role',
-            'Update Employee Manager',
-            'View All Roles',
-            'Add Role',
-            'Remove Role',
-            'View All Deparments',
-            'Add Deparment',
-            'Remove Deparment',
-            'View Total Utilized Budget By Department',
+            // 'View All Employees By Department',
+            // 'View All Employees By Manager',
+            // 'Add Employee',
+            // 'Remove Employee',
+            // 'Update Employee Role',
+            // 'Update Employee Manager',
+            // 'View All Roles',
+            // 'Add Role',
+            // 'Remove Role',
+            // 'View All Deparments',
+            // 'Add Deparment',
+            // 'Remove Deparment',
+            // 'View Total Utilized Budget By Department',
             'Quit'
         ]
     })
-}
+    .then((answers) => {
+        const { choices } = answers;
+
+        if (choices === 'View All Employees') {
+            renderAllEmployees();
+        }
+
+        // if (choices === 'View All Employees By Department') {
+        //     renderEmployeeDepartment();
+        // }
+
+        if (choices === 'Quit') {
+            dbConnect.end()
+        };
+    });
+};
+
+const renderAllEmployees = () => {
+    // console.log('Viewing All Employees');
+    // const sql = `SELECT employee.id, 
+    //                     employee.first_name, 
+    //                     employee.last_name, 
+    //                     role.title, 
+    //                     department.name AS department,
+    //                     role.salary, 
+    //                     CONCAT (manager.first_name, " ", manager.last_name) AS manager
+    //             FROM employee
+    //                     LEFT JOIN role ON employee.role_id = role.id
+    //                     LEFT JOIN department ON role.department_id = department.id
+    //                     LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+                        
+    // dbConnect.promise().query(sql, (err, rows) => {
+    //     if (err) throw err;
+
+    //     console.table(rows);
+
+    //     console.log('Viewed All Employees');
+
+    //     startDb();
+
+    // });
+
+    dbConnect.query (
+        `SELECT employee.id, 
+                employee.first_name, 
+                employee.last_name, 
+                role.title, 
+                department.name AS department,
+                role.salary, 
+                CONCAT (manager.first_name, " ", manager.last_name) AS manager
+        FROM employee
+                LEFT JOIN role ON employee.role_id = role.id
+                LEFT JOIN department ON role.department_id = department.id
+                LEFT JOIN employee manager ON employee.manager_id = manager.id`,
+
+                function (err, results, fields) {
+                    if (err) {
+                        console.log(err.message);
+                        return;
+                    }
+
+                    console.table(results);
+                    startDb();
+                }
+    );
+};
